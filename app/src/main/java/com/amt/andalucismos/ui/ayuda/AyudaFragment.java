e8183ed854cf5a1ce3e1e80cbd55ced2f6683e88
Alvaro <amtsalar@gmail.com>
2024-06-04 22:38:27 +0200

    AyudaFragment funcional. Notificaciones incompletas.

--- app/src/main/java/com/amt/andalucismos/ui/ayuda/AyudaFragment.java
+++ app/src/main/java/com/amt/andalucismos/ui/ayuda/AyudaFragment.java
@@ -7,15 +7,28 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 import com.amt.andalucismos.R;
 
 public class AyudaFragment extends Fragment {
 
+    private WebView webViewAyuda;
+
+    public AyudaFragment() {
+
+    } // constructor
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ayuda, container, false);
-    }
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    } // onCreate()
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_ayuda, container, false);
+        webViewAyuda = view.findViewById(R.id.webViewAyuda);
+        webViewAyuda.loadUrl("file:///android_asset/Ayuda_Andalucismos.html");
+        return view;
+    } // onCreateView()
 }
